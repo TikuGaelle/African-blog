1f6fa031cb480784a2b89f21f9cd481618786cc0
TikuGaelle <brendagaelle6@gmail.com>
2023-12-08 22:47:10 +0100

    updated mobile view

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,6 +2,7 @@ import Button from "./button";
 import classes from "./footer.module.css";
 import { ReactComponent as Twit } from "../images/twitter.svg";
 import { ReactComponent as FaceB } from "../images/facebook.svg";
+import { ReactComponent as LinkIn } from "../images/linkedin.svg";
 import Logo from "../images/logo.png";
 
 const Footer = () => {
@@ -20,23 +21,24 @@ const Footer = () => {
             <li>Cultures</li>
             <li>Landscapes</li>
             <br />
-            <Button style={style} text="Discover Africa" />
+            <Button
+              className={classes.footerBtn}
+              style={style}
+              text="Discover Africa"
+            />
           </div>
         </div>
         <div className={classes.emptyCon}>
           <div className={classes.sizedbox}></div>
           <div className={classes.socialLinks}>
-            <a
-              href="https://twitter.com/wakamann_?t=KEzF66_yXfIKsQilw-TUkQ&s=09"
-              target="_blank"
-            >
+            <a href="https://facebook.com/" target="_blank">
               <FaceB title="Our facebook page" className={classes.fb} />
             </a>
-            <a
-              href="https://twitter.com/wakamann_?t=KEzF66_yXfIKsQilw-TUkQ&s=09"
-              target="_blank"
-            >
+            <a href="https://twitter.com/" target="_blank">
               <Twit title="Our twitter page" className={classes.twiIco} />
+            </a>
+            <a href="https://linkedin.com/" target="_blank">
+              <LinkIn title="Our Linkedin page" className={classes.linkco} />
             </a>
           </div>
         </div>
